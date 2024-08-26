ff14d308b8b11ab9b900969fe1337deab8c07283
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2024-08-26 15:19:14 +0200

    init

--- chat_app.py
+++ chat_app.py
@@ -21,8 +21,20 @@ from openai.types.beta.threads.runs.code_interpreter_tool_call import (
     )
 
 # Set page config
-st.set_page_config(page_title="DAVE",
+st.set_page_config(page_title="Vedic GPT Chat",
                    layout='wide')
+
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
 
 # Get secrets
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
@@ -67,40 +79,45 @@ def moderation_endpoint(text) -> bool:
     return response.results[0].flagged
 
 # UI
-st.subheader("🔮 DAVE: Data Analysis & Visualisation Engine")
+st.subheader("🔮 Ask a question to Shrila Prabhupada's books.")
+
+# add page reload button:
+if st.button("♻️ New chat"):
+    st.experimental_rerun()
+
 file_upload_box = st.empty()
 upload_btn = st.empty()
 
-# Upload a file
-# File Upload
-if not st.session_state["file_uploaded"]:
-    st.session_state["files"] = file_upload_box.file_uploader("Please upload your dataset(s)",
-                                                              accept_multiple_files=True,
-                                                              type=["csv"])
+# # Upload a file
+# # File Upload
+# if not st.session_state["file_uploaded"]:
+#     st.session_state["files"] = file_upload_box.file_uploader("Please upload your dataset(s)",
+#                                                               accept_multiple_files=True,
+#                                                               type=["csv"])
+#
+#     if upload_btn.button("Upload"):
+#
+#         st.session_state["file_id"] = []
+#
+#         # Upload the file
+#         for file in st.session_state["files"]:
+#             oai_file = client.files.create(
+#                 file=file,
+#                 purpose='assistants'
+#             )
+#
+#             # Append the file ID to the list
+#             st.session_state["file_id"].append(oai_file.id)
+#             print(f"Uploaded new file: \t {oai_file.id}")
+#
+#         st.toast("File(s) uploaded successfully", icon="🚀")
+#         st.session_state["file_uploaded"] = True
+#         file_upload_box.empty()
+#         upload_btn.empty()
+#         # The re-run is to trigger the next section of the code
+#         st.rerun()
 
-    if upload_btn.button("Upload"):
-
-        st.session_state["file_id"] = []
-
-        # Upload the file
-        for file in st.session_state["files"]:
-            oai_file = client.files.create(
-                file=file,
-                purpose='assistants'
-            )
-
-            # Append the file ID to the list
-            st.session_state["file_id"].append(oai_file.id)
-            print(f"Uploaded new file: \t {oai_file.id}")
-
-        st.toast("File(s) uploaded successfully", icon="🚀")
-        st.session_state["file_uploaded"] = True
-        file_upload_box.empty()
-        upload_btn.empty()
-        # The re-run is to trigger the next section of the code
-        st.rerun()
-
-if st.session_state["file_uploaded"]:
+if st.session_state["file_uploaded"] or True:
 
     # Create a new thread
     if "thread_id" not in st.session_state:
@@ -111,7 +128,7 @@ if st.session_state["file_uploaded"]:
     # Update the thread to attach the file
     client.beta.threads.update(
             thread_id=st.session_state.thread_id,
-            tool_resources={"code_interpreter": {"file_ids": [file_id for file_id in st.session_state.file_id]}}
+            #tool_resources={"code_interpreter": {"file_ids": [file_id for file_id in st.session_state.file_id]}}
             )
 
     # Local history
@@ -135,7 +152,7 @@ if st.session_state["file_uploaded"]:
                     with st.status("Results", state="complete"):
                         st.code(item["content"])
 
-    if prompt := st.chat_input("Ask me a question about your dataset"):
+    if prompt := st.chat_input("Describe your query in detail. There will be no clarifying questions."):
         if moderation_endpoint(prompt):
             st.toast("Your message was flagged. Please try again.", icon="⚠️")
             st.stop
@@ -159,7 +176,7 @@ if st.session_state["file_uploaded"]:
             stream = client.beta.threads.runs.create(
                 thread_id=st.session_state.thread_id,
                 assistant_id=ASSISTANT_ID,
-                tool_choice={"type": "code_interpreter"},
+                #tool_choice={"type": "code_interpreter"},
                 stream=True
             )
 
@@ -167,65 +184,66 @@ if st.session_state["file_uploaded"]:
 
             for event in stream:
                 print(event)
-                if isinstance(event, ThreadRunStepCreated):
-                    if event.data.step_details.type == "tool_calls":
-                        assistant_output.append({"type": "code_input",
-                                                "content": ""})
-
-                        code_input_expander= st.status("Writing code ⏳ ...", expanded=True)
-                        code_input_block = code_input_expander.empty()
-
-                if isinstance(event, ThreadRunStepDelta):
-                    if event.data.delta.step_details.tool_calls[0].code_interpreter is not None:
-                        code_interpretor = event.data.delta.step_details.tool_calls[0].code_interpreter
-                        code_input_delta = code_interpretor.input
-                        if (code_input_delta is not None) and (code_input_delta != ""):
-                            assistant_output[-1]["content"] += code_input_delta
-                            code_input_block.empty()
-                            code_input_block.code(assistant_output[-1]["content"])
-
-                elif isinstance(event, ThreadRunStepCompleted):
-                    if isinstance(event.data.step_details, ToolCallsStepDetails):
-                        code_interpretor = event.data.step_details.tool_calls[0].code_interpreter
-                        if code_interpretor.outputs is not None:
-                            code_interpretor_outputs = code_interpretor.outputs[0]
-                            code_input_expander.update(label="Code", state="complete", expanded=False)
-                            # Image
-                            if isinstance(code_interpretor_outputs, CodeInterpreterOutputImage):
-                                image_html_list = []
-                                for output in code_interpretor.outputs:
-                                    image_file_id = output.image.file_id
-                                    image_data = client.files.content(image_file_id)
-                                    
-                                    # Save file
-                                    image_data_bytes = image_data.read()
-                                    with open(f"images/{image_file_id}.png", "wb") as file:
-                                        file.write(image_data_bytes)
-
-                                    # Open file and encode as data
-                                    file_ = open(f"images/{image_file_id}.png", "rb")
-                                    contents = file_.read()
-                                    data_url = base64.b64encode(contents).decode("utf-8")
-                                    file_.close()
-
-                                    # Display image
-                                    image_html = f'<p align="center"><img src="data:image/png;base64,{data_url}" width=600></p>'
-                                    st.html(image_html)
-
-                                    image_html_list.append(image_html)
-
-                                assistant_output.append({"type": "image",
-                                                        "content": image_html_list})
-                            # Console log
-                            elif isinstance(code_interpretor_outputs, CodeInterpreterOutputLogs):
-                                assistant_output.append({"type": "code_output",
-                                                         "content": ""})
-                                code_output = code_interpretor.outputs[0].logs
-                                with st.status("Results", state="complete"):
-                                    st.code(code_output)    
-                                    assistant_output[-1]["content"] = code_output   
-
-                elif isinstance(event, ThreadMessageCreated):
+#                 if isinstance(event, ThreadRunStepCreated):
+#                     if event.data.step_details.type == "tool_calls":
+#                         assistant_output.append({"type": "code_input",
+#                                                 "content": ""})
+#
+#                         code_input_expander= st.status("Writing code ⏳ ...", expanded=True)
+#                         code_input_block = code_input_expander.empty()
+#
+#                 if isinstance(event, ThreadRunStepDelta):
+#                     if event.data.delta.step_details.tool_calls[0].code_interpreter is not None:
+#                         code_interpretor = event.data.delta.step_details.tool_calls[0].code_interpreter
+#                         code_input_delta = code_interpretor.input
+#                         if (code_input_delta is not None) and (code_input_delta != ""):
+#                             assistant_output[-1]["content"] += code_input_delta
+#                             code_input_block.empty()
+#                             code_input_block.code(assistant_output[-1]["content"])
+#
+#                 elif isinstance(event, ThreadRunStepCompleted):
+#                     if isinstance(event.data.step_details, ToolCallsStepDetails):
+#                         code_interpretor = event.data.step_details.tool_calls[0].code_interpreter
+#                         if code_interpretor.outputs is not None:
+#                             code_interpretor_outputs = code_interpretor.outputs[0]
+#                             code_input_expander.update(label="Code", state="complete", expanded=False)
+#                             # Image
+#                             if isinstance(code_interpretor_outputs, CodeInterpreterOutputImage):
+#                                 image_html_list = []
+#                                 for output in code_interpretor.outputs:
+#                                     image_file_id = output.image.file_id
+#                                     image_data = client.files.content(image_file_id)
+#
+#                                     # Save file
+#                                     image_data_bytes = image_data.read()
+#                                     with open(f"images/{image_file_id}.png", "wb") as file:
+#                                         file.write(image_data_bytes)
+#
+#                                     # Open file and encode as data
+#                                     file_ = open(f"images/{image_file_id}.png", "rb")
+#                                     contents = file_.read()
+#                                     data_url = base64.b64encode(contents).decode("utf-8")
+#                                     file_.close()
+#
+#                                     # Display image
+#                                     image_html = f'<p align="center"><img src="data:image/png;base64,{data_url}" width=600></p>'
+#                                     st.html(image_html)
+#
+#                                     image_html_list.append(image_html)
+#
+#                                 assistant_output.append({"type": "image",
+#                                                         "content": image_html_list})
+#                             # Console log
+#                             elif isinstance(code_interpretor_outputs, CodeInterpreterOutputLogs):
+#                                 assistant_output.append({"type": "code_output",
+#                                                          "content": ""})
+#                                 code_output = code_interpretor.outputs[0].logs
+#                                 with st.status("Results", state="complete"):
+#                                     st.code(code_output)
+#                                     assistant_output[-1]["content"] = code_output
+#
+#                elif isinstance(event, ThreadMessageCreated):
+                if isinstance(event, ThreadMessageCreated):
                     assistant_output.append({"type": "text",
                                             "content": ""})
                     assistant_text_box = st.empty()
