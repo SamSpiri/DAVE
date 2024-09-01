ee8d9a1ee17c148795a515fc19b654b7d393a846
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2024-09-01 20:54:47 +0200

    init

--- chat_app.py
+++ chat_app.py
@@ -3,7 +3,7 @@ chat_app.py
 """
 import base64
 import os
-
+import hmac
 import streamlit as st
 from openai import OpenAI
 from openai.types.beta.assistant_stream_event import (
@@ -11,7 +11,8 @@ from openai.types.beta.assistant_stream_event import (
     ThreadRunStepDelta,
     ThreadRunStepCompleted,
     ThreadMessageCreated,
-    ThreadMessageDelta
+    ThreadMessageDelta,
+    ThreadMessageCompleted
     )
 from openai.types.beta.threads.text_delta_block import TextDeltaBlock 
 from openai.types.beta.threads.runs.tool_calls_step_details import ToolCallsStepDetails
@@ -19,6 +20,35 @@ from openai.types.beta.threads.runs.code_interpreter_tool_call import (
     CodeInterpreterOutputImage,
     CodeInterpreterOutputLogs
     )
+
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # Set page config
 st.set_page_config(page_title="Vedic GPT Chat",
@@ -84,7 +114,7 @@ st.subheader("🔮 Ask a question to Shrila Prabhupada's books.")
 # add page reload button:
 if st.button("♻️ New chat"):
     st.session_state.clear()
-    st.experimental_rerun()
+    st.rerun()
 
 file_upload_box = st.empty()
 upload_btn = st.empty()
@@ -154,9 +184,9 @@ if st.session_state["file_uploaded"] or True:
                         st.code(item["content"])
 
     if prompt := st.chat_input("Describe your query in detail. There will be no clarifying questions."):
-        if moderation_endpoint(prompt):
-            st.toast("Your message was flagged. Please try again.", icon="⚠️")
-            st.stop
+        #if moderation_endpoint(prompt):
+        #    st.toast("Your message was flagged. Please try again.", icon="⚠️")
+        #    st.stop
 
         st.session_state.messages.append({"role": "user",
                                         "items": [
@@ -167,7 +197,7 @@ if st.session_state["file_uploaded"] or True:
         client.beta.threads.messages.create(
             thread_id=st.session_state.thread_id,
             role="user",
-            content=prompt
+            content=f"Используй файлы и приводи полные цитаты: {prompt}"
         )
 
         with st.chat_message("user"):
@@ -250,9 +280,54 @@ if st.session_state["file_uploaded"] or True:
                     assistant_text_box = st.empty()
 
                 elif isinstance(event, ThreadMessageDelta):
-                    if isinstance(event.data.delta.content[0], TextDeltaBlock):
+                    delta = event.data.delta.content[0]
+                    if isinstance(delta, TextDeltaBlock):
                         assistant_text_box.empty()
-                        assistant_output[-1]["content"] += event.data.delta.content[0].text.value
+                        annotations = delta.text.annotations
+                        if annotations is not None and len(annotations):
+                            for annotation in annotations:
+                                assistant_output[-1]["content"] += f" [{annotation.index+1}] "
+                        else:
+                            assistant_output[-1]["content"] += delta.text.value
                         assistant_text_box.markdown(assistant_output[-1]["content"])
-                
+
+                elif isinstance(event, ThreadMessageCompleted):
+                    annotations = event.data.content[0].text.annotations
+                    if annotations is not None and len(annotations):
+                        assistant_output[-1]["content"] += f"\n\nFiles:\n"
+                        index = 0
+                        for annotation in annotations:
+                            index += 1
+                            #message_content.value = message_content.value.replace(
+                            #    annotation.text, f"[{index}]"
+                            #)
+                            if file_citation := getattr(annotation, "file_citation", None):
+                                cited_file = client.files.retrieve(file_citation.file_id)
+                                assistant_output[-1]["content"] += f"- [{index}] {cited_file.filename}\n"
+                                assistant_text_box.markdown(assistant_output[-1]["content"])
+
+                elif isinstance(event, ThreadRunStepCompleted):
+                    if event.data.usage is not None:
+                        assistant_text_box = st.empty()
+                        assistant_output.append({"type": "text",
+                                                "content": f"{event.data.usage}"})
+                        assistant_text_box.markdown(assistant_output[-1]["content"])
+
             st.session_state.messages.append({"role": "assistant", "items": assistant_output})
+
+# ThreadMessageCompleted(data=Message(id='msg_iBHSNLIkwEkAmc4jtOsXciMJ', assistant_id='asst_Mur6zlOfYl9kwmc9EYJHud2p', attachments=[], completed_at=1725141840,
+#content=[TextContentBlock(text=Text(annotations=[FileCitationAnnotation(end_index=389, file_citation=FileCitation(file_id='file-4YD0SwCRVMU9YoUvogd6Pnnu',
+# quote=None), start_index=376, text='【4:15†source】', type='file_citation'), FileCitationAnnotation(end_index=746,
+# file_citation=FileCitation(file_id='file-sWzPeWCMdrqlrcMyd4NGMw29', quote=None), start_index=734, text='【4:7†source】', type='file_citation'),
+#  FileCitationAnnotation(end_index=1135, file_citation=FileCitation(file_id='file-MO1yxNzQqxIAuRLdEROFfQV6', quote=None), start_index=1123, text='【4:1†source】',
+#   type='file_citation'), FileCitationAnnotation(end_index=1419, file_citation=FileCitation(file_id='file-4YD0SwCRVMU9YoUvogd6Pnnu', quote=None),
+#   start_index=1407, text='【4:4†source】', type='file_citation'), FileCitationAnnotation(end_index=1766,
+#   file_citation=FileCitation(file_id='file-MO1yxNzQqxIAuRLdEROFfQV6', quote=None), start_index=1754, text='【4:1†source】', type='file_citation'),
+#    FileCitationAnnotation(end_index=2014, file_citation=FileCitation(file_id='file-9AoM6vyBUjsxV6kvOKDnym5s', quote=None),
+#    start_index=2001, text='【4:16†source】', type='file_citation')],
+#    value='Чтобы научиться предсказывать будущее, важно понимать несколько ключевых аспектов, основанных на ведическом знании и учениях Шрилы Прабхупады.\n\n1. **Знание о времени и карме**: Веды учат, что наше будущее зависит от наших действий в прошлом и настоящем. Как сказано в «Шримад-Бхагаватам», "в зависимости от степени осквернения тремя гунами человек пребывает в трех состояниях"【4:15†source】. Это означает, что наше поведение и поступки формируют нашу судьбу.\n\n2. **Астрология как инструмент**: Астрология — это один из способов предсказания будущего, который использует расположение небесных тел. В «Шримад-Бхагаватам» упоминается, что "производя расчеты, связанные с расположением светил, можно предсказать, что ожидает нас в будущем"【4:7†source】. Астрологи могут использовать свои знания, чтобы определить, какие события могут произойти в жизни человека, основываясь на его гороскопе.\n\n3. **Духовная практика**: Освобожденные души, такие как великий мудрец Вьясадева, могут предсказывать будущее благодаря своей духовной проницательности, которая достигается через практику преданного служения и изучение священных писаний【4:1†source】. Это подчеркивает важность духовного роста и практики в понимании будущего.\n\n4. **Понимание природы времени**: Веды учат, что время осквернено тремя гунами (благостью, страстью и невежеством), и наше восприятие времени и событий зависит от того, в какой гуне мы находимся【4:4†source】. Осознание этого может помочь нам лучше понять, как наши действия влияют на наше будущее.\n\n5. **Служение и сострадание**: Истинные преданные, такие как Шрила Прабхупада, стремятся служить другим и помогать им. Это служение не только приносит благо другим, но и очищает душу, позволяя лучше понимать свою судьбу и предсказывать будущее【4:1†source】.\n\n6. **Смирение и преданность**: Важно помнить, что предсказание будущего — это не просто механический процесс. Оно требует смирения и преданности Богу. Как говорит Господь Кришна в «Бхагавад-гите», "Мой преданный никогда не погибнет"【4:16†source】. Это подчеркивает, что преданность и служение Богу могут привести к лучшему пониманию своей судьбы.\n\nТаким образом, чтобы научиться предсказывать будущее, необходимо сочетать знание о карме, изучение астрологии, духовную практику, понимание времени и служение другим. Это путь, который требует времени и усилий, но он ведет к глубокому пониманию жизни и своего места в ней.'),
+#     type='text')], created_at=1725141828, incomplete_at=None, incomplete_details=None, metadata={}, object='thread.message',
+#     role='assistant', run_id='run_wpP8tkSdWrQTbpCeIfJ2Qogm', status='completed', thread_id='thread_2c7IG9eGLIJfJWNlmXDEKnSr'), event='thread.message.completed')
+
+
+
